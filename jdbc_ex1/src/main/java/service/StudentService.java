c2af0d13e1beba32b8dead956eb980e5c398e6cc
seonghee-0 <seongheee0@gmail.com>
2024-06-25 18:16:08 +0900

    Delete jdbc_ex1 directory

--- jdbc_ex1/src/main/java/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import common.ActionForward;
-import jakarta.servlet.http.HttpServletRequest;
-
-
-public interface StudentService {
-  ActionForward getStudentList(HttpServletRequest request);
-}
